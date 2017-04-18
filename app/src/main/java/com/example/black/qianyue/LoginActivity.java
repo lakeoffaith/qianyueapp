fb358dd4739ffd5d1518a24ddd43900903f97e7a
lakeoffaith <905663280@qq.com>
2017-04-18 16:52:48 +0800

    first commit

---
+++ app/src/main/java/com/example/black/qianyue/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.black.qianyue;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ app/src/main/java/com/example/black/qianyue/MainFragment.java
@@ -0,0 +1,46 @@
+package com.example.black.qianyue;
+
+import android.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainFragment extends Fragment {
+    private static final String TAB_ID="contentId";
+
+
+    private String content;
+
+    public static final MainFragment newInstance(String content)
+    {
+        MainFragment f = new MainFragment();
+        Bundle bdl = new Bundle(2);
+        bdl.putString(TAB_ID, content);
+        f.setArguments(bdl);
+        return f;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.content=getArguments().getString(TAB_ID);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view= inflater.inflate(R.layout.fragment_main, container, false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+
+
+
+
+}
